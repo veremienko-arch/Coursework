39b520a118570692c2c0d6cf0e87886ac7564ef4
Veremiienko <veremienko.rr@gmail.com>
2026-04-11 21:43:48 +0300

    Base structure

--- main.cpp
+++ main.cpp
@@ -1,4 +1,42 @@
 #include <iostream>
+#include <vector>
+
+enum Direction {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
+};
+
+enum CircleType {
+    BLANK,
+    WHITE,
+    BLACK
+};
+
+enum LineStatus {
+    UNKNOWN,
+    LINE,
+    EMPTY
+};
+
+struct Node {
+    CircleType circle = CircleType::BLANK;
+
+    LineStatus up = LineStatus::UNKNOWN;
+    LineStatus down = LineStatus::UNKNOWN;
+    LineStatus right = LineStatus::UNKNOWN;
+    LineStatus left = LineStatus::UNKNOWN;
+};
+
+class MasyuCore {
+private:
+    int width, height;
+    std::vector<std::vector<Node>> grid;
+
+public:
+    MasyuCore(int width, int height);
+};
 
 int main() {
 
