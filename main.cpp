87e35cf2951d9ec55ae2f0c09f4ae83df67e157f
Veremiienko <veremienko.rr@gmail.com>
2026-04-24 20:42:18 +0300

    SetLineStatus + improved PrintToConsole (+ some tests)

--- main.cpp
+++ main.cpp
@@ -35,8 +35,7 @@ private:
     std::vector<std::vector<Node>> grid;
     std::vector<std::vector<std::vector<Node>>> history;
 
-    //bool ifCoordIsValid(int x, int y) const; seems useless now
-    void setLineStatus(int x, int y, Direction dir, LineStatus status);
+    bool ifCoordIsValid(int x, int y) const;
     void saveState();
     void restoreState();
 
@@ -48,6 +47,7 @@ public:
     MasyuSolverCore(int x, int y);
 
     void setCircleType(int x,  int y, CircleType type);
+    void setLineStatus(int x, int y, Direction dir, LineStatus status);
 
     //for Qt
     int getWidth() const { return width; }
@@ -80,9 +80,64 @@ void MasyuSolverCore::printToConsole() const {
             } else {
                 std::cout << "+";
             }
-            std::cout << "   ";
+
+            if (x < width - 1) {
+                if (grid[y][x].right == LineStatus::LINE /*|| grid[y][x].left == LineStatus::LINE*/) {
+                    std::cout << "---";
+                } else {
+                    std::cout << "   ";
+                }
+            }
         }
         std::cout << std::endl;
+
+        if (y < height - 1) {
+            for (int x = 0; x < width; x++) {
+                if (grid[y][x].down == LineStatus::LINE /*|| grid[y][x].up == LineStatus::LINE*/) {
+                    std::cout << "|   ";
+                } else {
+                  std::cout << "    ";
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
+}
+
+bool MasyuSolverCore::ifCoordIsValid(int x, int y) const {
+    return x >= 0 && x < width && y >= 0 && y < height;
+}
+
+void MasyuSolverCore::setLineStatus(int x, int y, Direction dir, LineStatus status) {
+    if (!ifCoordIsValid(x, y)) return;
+
+    switch (dir) {
+        case Direction::UP: grid[y][x].up = status; break;
+        case Direction::DOWN: grid[y][x].down = status; break;
+        case Direction::RIGHT: grid[y][x].right = status; break;
+        case Direction::LEFT: grid[y][x].left = status; break;
+        default: break;
+    }
+
+    int nx = x, ny = y;
+    Direction oppositeDirection;
+
+    switch (dir) {
+        case Direction::UP: ny = y - 1; oppositeDirection = Direction::DOWN; break;
+        case Direction::DOWN: ny = y + 1; oppositeDirection = Direction::UP; break;
+        case Direction::LEFT: nx = x - 1; oppositeDirection = Direction::RIGHT; break;
+        case Direction::RIGHT: nx = x + 1; oppositeDirection = Direction::LEFT; break;
+        default: break;
+    }
+
+    if (ifCoordIsValid(nx, ny)) {
+        switch (oppositeDirection) {
+            case Direction::UP: grid[ny][nx].up = status; break;
+            case Direction::DOWN: grid[ny][nx].down = status; break;
+            case Direction::LEFT: grid[ny][nx].left = status; break;
+            case Direction::RIGHT: grid[ny][nx].right = status; break;
+            default: break;
+        }
     }
 }
 
@@ -117,6 +172,11 @@ int main() {
     field1.setCircleType(9, 8, CircleType::WHITE);
     field1.setCircleType(2, 9, CircleType::WHITE);
     field1.setCircleType(11, 9, CircleType::WHITE);
+
+    field1.setLineStatus(4, 0, Direction::RIGHT, LineStatus::LINE); //test
+    field1.setLineStatus(3, 5, Direction::DOWN, LineStatus::LINE); // test
+    field1.setLineStatus(5, 1, Direction::LEFT, LineStatus::LINE); //test
+    field1.setLineStatus(1, 7, Direction::UP, LineStatus::LINE); //test
 
     std::cout << "Current grid  of Ex. #1 (unsolved):" << std::endl;
 
