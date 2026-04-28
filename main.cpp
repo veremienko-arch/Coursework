8801f773c6e65493fc9d7dc798a76999e63040ac
Veremiienko <veremienko.rr@gmail.com>
2026-04-28 19:42:09 +0300

    Complete nodeRules + visual improvements.

--- main.cpp
+++ main.cpp
@@ -1,115 +1,105 @@
 #include <iostream>
 #include <vector>
 
+using std::cout;
+using std::endl;
+
 enum class Direction {
-    UP,
-    DOWN,
-    RIGHT,
-    LEFT
+    UP, DOWN, RIGHT, LEFT
 };
 
 enum class CircleType {
-    BLANK,
-    WHITE,
-    BLACK
+    EMPTY, WHITE, BLACK
 };
 
-enum class LineStatus {
-    UNKNOWN,
-    LINE,
-    EMPTY
+enum class LineStat {
+    UNKNOWN, LINE, EMPTY
 };
 
 struct Node {
-    CircleType circle = CircleType::BLANK;
-
-    LineStatus up = LineStatus::UNKNOWN;
-    LineStatus down = LineStatus::UNKNOWN;
-    LineStatus right = LineStatus::UNKNOWN;
-    LineStatus left = LineStatus::UNKNOWN;
+    CircleType circle = CircleType::EMPTY;
+    LineStat up = LineStat::UNKNOWN;
+    LineStat down = LineStat::UNKNOWN;
+    LineStat right = LineStat::UNKNOWN;
+    LineStat left = LineStat::UNKNOWN;
 };
 
-class MasyuSolverCore {
+class CourseworkMasyu {
 private:
     int width, height;
     std::vector<std::vector<Node>> grid;
     std::vector<std::vector<std::vector<Node>>> history;
 
-    bool ifCoordIsValid(int x, int y) const;
+    bool ifCoordValid(int x, int y) const;
+    bool nodeRules(int x, int y);
     void saveState();
     void restoreState();
-
     void applyRules();
     bool backtrack();
-    bool ifLoopIsComplete() const;
+    bool ifLoopComplete() const;
 
 public:
-    MasyuSolverCore(int x, int y);
-
+    CourseworkMasyu(int x, int y);
     void setCircleType(int x,  int y, CircleType type);
-    void setLineStatus(int x, int y, Direction dir, LineStatus status);
+    void setLineStatus(int x, int y, Direction dir, LineStat status);
 
-    //for Qt
     int getWidth() const { return width; }
     int getHeight() const { return height; }
     CircleType getCircleType(int x, int y) const;
-    LineStatus getLine(int  x, int y, Direction dir) const;
+    LineStat getLine(int  x, int y, Direction dir) const;
 
     bool solve();
     void printToConsole() const;
 };
 
-MasyuSolverCore::MasyuSolverCore(int x, int y) {
+CourseworkMasyu::CourseworkMasyu(int x, int y) {
     width = x;
     height = y;
-
     grid.resize(height, std::vector<Node>(width));
 }
 
-void MasyuSolverCore::setCircleType(int x, int y, CircleType type) {
+void CourseworkMasyu::setCircleType(int x, int y, CircleType type) {
     grid[y][x].circle = type;
 }
 
-void MasyuSolverCore::printToConsole() const {
+void CourseworkMasyu::printToConsole() const {
+    cout << std::string(53, '-') << endl;
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            if (grid[y][x].circle == CircleType::WHITE) {
-                std::cout << "0";
-            } else if (grid[y][x].circle == CircleType::BLACK) {
-                std::cout << "#";
-            } else {
-                std::cout << "+";
+            switch (grid[y][x].circle) {
+                case CircleType::WHITE: cout << '0'; break;
+                case CircleType::BLACK: cout << '#'; break;
+                default: cout << "+"; break;
             }
-
             if (x < width - 1) {
-                if (grid[y][x].right == LineStatus::LINE /*|| grid[y][x].left == LineStatus::LINE*/) {
-                    std::cout << "---";
+                if (grid[y][x].right == LineStat::LINE) {
+                    cout << "---";
                 } else {
-                    std::cout << "   ";
+                    cout << "   ";
                 }
             }
         }
-        std::cout << std::endl;
-
+        cout << endl;
         if (y < height - 1) {
             for (int x = 0; x < width; x++) {
-                if (grid[y][x].down == LineStatus::LINE /*|| grid[y][x].up == LineStatus::LINE*/) {
-                    std::cout << "|   ";
+                if (grid[y][x].down == LineStat::LINE) {
+                    cout << "|   ";
                 } else {
-                  std::cout << "    ";
+                  cout << "    ";
                 }
             }
-            std::cout << std::endl;
+            cout << endl;
         }
     }
+    cout << std::string(53, '-') << endl;
 }
 
-bool MasyuSolverCore::ifCoordIsValid(int x, int y) const {
+bool CourseworkMasyu::ifCoordValid(int x, int y) const {
     return x >= 0 && x < width && y >= 0 && y < height;
 }
 
-void MasyuSolverCore::setLineStatus(int x, int y, Direction dir, LineStatus status) {
-    if (!ifCoordIsValid(x, y)) return;
+void CourseworkMasyu::setLineStatus(int x, int y, Direction dir, LineStat status) {
+    if (!ifCoordValid(x, y)) return;
 
     switch (dir) {
         case Direction::UP: grid[y][x].up = status; break;
@@ -130,7 +120,7 @@ void MasyuSolverCore::setLineStatus(int x, int y, Direction dir, LineStatus stat
         default: break;
     }
 
-    if (ifCoordIsValid(nx, ny)) {
+    if (ifCoordValid(nx, ny)) {
         switch (oppositeDirection) {
             case Direction::UP: grid[ny][nx].up = status; break;
             case Direction::DOWN: grid[ny][nx].down = status; break;
@@ -141,12 +131,99 @@ void MasyuSolverCore::setLineStatus(int x, int y, Direction dir, LineStatus stat
     }
 }
 
-// could be useful -> std::vector<Grid> history;
+bool CourseworkMasyu::nodeRules(int x, int y) {
+    int lines = 0;
+    int unknowns = 0;
+    bool changed = false;
+
+    switch (grid[y][x].up) {
+        case LineStat::LINE: lines++; break;
+        case LineStat::UNKNOWN: unknowns++; break;
+    }
+    switch (grid[y][x].right) {
+        case LineStat::LINE: lines++; break;
+        case LineStat::UNKNOWN: unknowns++; break;
+    }
+    switch (grid[y][x].down) {
+        case LineStat::LINE: lines++; break;
+        case LineStat::UNKNOWN: unknowns++; break;
+    }
+    switch (grid[y][x].left) {
+        case LineStat::LINE: lines++; break;
+        case LineStat::UNKNOWN: unknowns++; break;
+    }
+
+    if (lines == 2 && unknowns > 0) {
+        if (grid[y][x].up == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::UP, LineStat::EMPTY);
+            changed = true;
+        }
+        if (grid[y][x].right == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::RIGHT, LineStat::EMPTY);
+            changed = true;
+        }
+        if (grid[y][x].down == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::DOWN, LineStat::EMPTY);
+            changed = true;
+        }
+        if (grid[y][x].left == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::LEFT, LineStat::EMPTY);
+            changed = true;
+        }
+    }
+
+    if (lines == 1 && unknowns == 1) {
+        if (grid[y][x].up == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::UP, LineStat::LINE);
+            changed = true;
+        }
+        if (grid[y][x].right == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::RIGHT, LineStat::LINE);
+            changed = true;
+        }
+        if (grid[y][x].down == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::DOWN, LineStat::LINE);
+            changed = true;
+        }
+        if (grid[y][x].left == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::LEFT, LineStat::LINE);
+            changed = true;
+        }
+    }
+
+    if (lines == 0 && unknowns == 1) {
+        if (grid[y][x].up == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::UP, LineStat::EMPTY);
+            changed = true;
+        }
+        if (grid[y][x].right == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::RIGHT, LineStat::EMPTY);
+            changed = true;
+        }
+        if (grid[y][x].down == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::DOWN, LineStat::EMPTY);
+            changed = true;
+        }
+        if (grid[y][x].left == LineStat::UNKNOWN) {
+            setLineStatus(x, y, Direction::LEFT, LineStat::EMPTY);
+            changed = true;
+        }
+    }
+
+    return changed;
+}
+
+
+
+void CourseworkMasyu::applyRules() {
+
+}
+
 int main() {
     int w = 14;
     int h = 10;
-    MasyuSolverCore field1(w, h);
-    // EXAMPLE #1
+    CourseworkMasyu field1(w, h);
+
     field1.setCircleType(4, 0, CircleType::WHITE);
     field1.setCircleType(7, 0, CircleType::BLACK);
     field1.setCircleType(2, 2, CircleType::WHITE);
@@ -173,12 +250,7 @@ int main() {
     field1.setCircleType(2, 9, CircleType::WHITE);
     field1.setCircleType(11, 9, CircleType::WHITE);
 
-    field1.setLineStatus(4, 0, Direction::RIGHT, LineStatus::LINE); //test
-    field1.setLineStatus(3, 5, Direction::DOWN, LineStatus::LINE); // test
-    field1.setLineStatus(5, 1, Direction::LEFT, LineStatus::LINE); //test
-    field1.setLineStatus(1, 7, Direction::UP, LineStatus::LINE); //test
-
-    std::cout << "Current grid  of Ex. #1 (unsolved):" << std::endl;
+    cout << "Current grid  of Ex. #1 (unsolved):" << endl;
 
     field1.printToConsole();
 
